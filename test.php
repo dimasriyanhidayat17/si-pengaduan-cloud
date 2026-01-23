28c27ac04d3286e37731a8dfdf1b93bdddfde3fa
dimasriyanhidayat17 <dimasriyanhidayat01@gmail.com>
2026-01-24 00:15:04 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,23 @@
 <?php
-echo "PHP OK";
+$url = getenv("DATABASE_URL");
+
+if (!$url) {
+    die("❌ DATABASE_URL belum kebaca");
+}
+
+$db = parse_url($url);
+
+$conn = new mysqli(
+    $db["host"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/"),
+    $db["port"]
+);
+
+if ($conn->connect_error) {
+    die("❌ DB gagal: " . $conn->connect_error);
+}
+
+echo "✅ PHP + DATABASE Railway aman!";
+?>
