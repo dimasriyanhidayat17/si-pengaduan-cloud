1971612e1fe434170edda69042ae848b7264859c
dimasriyanhidayat17 <dimasriyanhidayat01@gmail.com>
2026-01-24 22:50:22 +0700

    Update db_test.php

--- db_test.php
+++ db_test.php
@@ -1,17 +1,2 @@
 <?php
-try {
-    $host = getenv("MYSQLHOST");
-    $db   = getenv("MYSQLDATABASE");
-    $user = getenv("MYSQLUSER");
-    $pass = getenv("MYSQLPASSWORD");
-    $port = getenv("MYSQLPORT");
-
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
-
-    $pdo = new PDO($dsn, $user, $pass);
-
-    echo "✅ Database Railway berhasil terkoneksi!";
-} catch (Exception $e) {
-    echo "❌ Error: " . $e->getMessage();
-}
-?>
+require "config/database.php";
