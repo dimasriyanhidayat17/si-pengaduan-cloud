ccf29d1542a4445ac60884c5a183d37d2d1b629e
dimasriyanhidayat17 <dimasriyanhidayat01@gmail.com>
2026-01-28 01:22:05 +0700

    Update init_admin.php

--- init_admin.php
+++ init_admin.php
@@ -1,7 +1,9 @@
 <?php
 require_once "config/database.php";
 
-$stmt = $pdo->prepare("INSERT INTO admin (username,password) VALUES (?,?)");
-$stmt->execute(["admin","admin123"]);
+$stmt = $pdo->prepare(
+    "INSERT INTO admin (username, password) VALUES (?, ?)"
+);
+$stmt->execute(["admin", "admin123"]);
 
-echo "✅ Admin berhasil dibuat!";
+echo "ADMIN BERHASIL DIBUAT";
